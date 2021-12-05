fd202c5364d0bdbc7c0ea77d2080eafab140caae
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-12-05 17:48:04 +0100

    feat(challenge01): complete challenge for solution 2

--- challenge-01/solution-2/js/script.js
+++ challenge-01/solution-2/js/script.js
@@ -7,6 +7,14 @@ const ovejas = [
 
 function contarOvejas(ovejas) {
 	// aquí tu magia
+
+	console.log("🐑 List sheep", ovejas);
+
+	const letras = ["n", "a"];
+	const elColor = "rojo";
+	ovejas = ovejas.filter(el => el.color === elColor && letras.every(i => el.name.toLowerCase().split('').includes(i)));
+	console.log("🐑 List of sheep filtered", ovejas);
+
 	return ovejas
 }
 
