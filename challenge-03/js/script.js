71dcab80099b9b7f3b504d0641b3c2d14c21aca6
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-12-05 22:19:16 +0100

    test(challenge03): create a random letter to check if it is valid

--- challenge-03/js/script.js
+++ challenge-03/js/script.js
@@ -1,4 +1,22 @@
-export default function isValid(letter) {
+const letterList = [
+	"bici coche (balón) bici coche peluche",
+	"(muñeca) consola bici",
+	"bici coche (balón bici coche",
+	"peluche (bici [coche) bici coche balón",
+	"(peluche {) bici",
+	"() bici"
+];
+
+const letter = letterList[Math.floor(Math.random() * letterList.length)];
+
+function isValid(letter) {
 	// ¡No dejes que el Grinch gane!
+
+	console.log("📜 Letter: ", letter);
+
 	return true
 }
+
+const test = isValid(letter)
+
+console.log(test)
