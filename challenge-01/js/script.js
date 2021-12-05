3b3acdf161a121113df9cf675bf8609752ed6959
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-12-05 16:15:57 +0100

    feat(challenge01): added description and files for work

---
+++ challenge-01/js/script.js
@@ -0,0 +1,15 @@
+const ovejas = [
+	{ name: 'Noa', color: 'azul' },
+	{ name: 'Euge', color: 'rojo' },
+	{ name: 'Navidad', color: 'rojo' },
+	{ name: 'Ki Na Ma', color: 'rojo' }
+];
+
+export default function contarOvejas(ovejas) {
+	// aquí tu magia
+	return ovejas
+}
+
+const ovejasFiltradas = contarOvejas(ovejas)
+
+console.log(ovejasFiltradas)
