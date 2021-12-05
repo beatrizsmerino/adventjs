551fca7877a6a8066bf0a449c01ed5671090ffaf
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-12-05 21:19:10 +0100

    feat(challenge02): added description and files for work

---
+++ challenge-02/js/script.js
@@ -0,0 +1,10 @@
+const carta = 'bici coche balón _playstation bici coche peluche'
+
+export default function listGifts(letter) {
+	// ¡Tú puedes!
+	return {};
+}
+
+const regalos = listGifts(carta)
+
+console.log(regalos)
