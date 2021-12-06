8157c0c2fd9c37557e25616c5c3259684a6505e7
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-12-06 21:38:59 +0100

    test(challenge06): run the function with different values to test it

--- challenge-06/js/script.js
+++ challenge-06/js/script.js
@@ -1,4 +1,15 @@
-export default function sumPairs(numbers, result) {
+function sumPairs(numbers, result) {
 	// And don't forget to share your solution on social media!
+
+	console.log("#️⃣ Numbers: ", numbers);
+	console.log("🧮 Result: ", result);
+
 	return null;
 }
+
+
+sumPairs([3, 5, 7, 2], 10); // [3, 7]
+sumPairs([-3, -2, 7, -5], 10); // null
+sumPairs([2, 2, 3, 1], 4); // [2, 2]
+sumPairs([6, 7, 1, 2], 8); // [6, 2]
+sumPairs([0, 2, 2, 3, -1, 1, 5], 6); // [1, 5]
