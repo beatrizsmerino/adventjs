2685502fa595f9debf0d0bf703bcb9b220c5ef9b
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-12-06 03:35:07 +0100

    test(challenge04): create a random height for print the tree

--- challenge-04/js/script.js
+++ challenge-04/js/script.js
@@ -1,4 +1,11 @@
-export default function createXmasTree(height) {
+function createXmasTree(height) {
 	// ¡Y no olvides también poner los turrones!
+	console.log("🎄 Height tree:", height);
 	return "";
 }
+
+function randomIntFromInterval(min, max) { // min and max included 
+	return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+createXmasTree(randomIntFromInterval(1, 100));
