66b1ae20900275c5fc687646be33a5a184fb5874
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2022-11-01 22:07:14 +0100

    feat(challenge08): working on the challenge

--- challenge-08/js/script.js
+++ challenge-08/js/script.js
@@ -1,9 +1,24 @@
-export default function maxProfit(prices) {
+function maxProfit(prices) {
 	// And don't forget to share your solution on networks!
 
 	console.log("🪙 Prices: ", prices);
 
-	return false
+	const priceMin = prices.sort((a, b) => a - b)[0];
+	console.log("The min price:", priceMin);
+
+	const priceMax = prices.sort((a, b) => b - a)[0];
+	console.log("The max price: ", priceMax);
+
+	const benefits = priceMax - priceMin;
+	console.log(`${benefits} -> (buy at ${priceMin}, sell at ${priceMax})`);
+
+	if (benefits > 0) {
+		console.log("profit possible");
+		return benefits;
+	} else {
+		console.log("no profit possible");
+		return -1;
+	}
 }
 
 const pricesBtc = [39, 18, 29, 25, 34, 32, 5];
