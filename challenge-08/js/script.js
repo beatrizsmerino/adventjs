81df422abdcedd97362316428541ad41bdbbffb5
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2022-11-01 17:45:43 +0100

    test(challenge08): run the function with different values to test it

--- challenge-08/js/script.js
+++ challenge-08/js/script.js
@@ -1,4 +1,19 @@
 export default function maxProfit(prices) {
 	// And don't forget to share your solution on networks!
+
+	console.log("🪙 Prices: ", prices);
+
 	return false
 }
+
+const pricesBtc = [39, 18, 29, 25, 34, 32, 5];
+maxProfit(pricesBtc);
+
+const pricesEth = [10, 20, 30, 40, 50, 60, 70];
+maxProfit(pricesEth);
+
+const pricesDoge = [18, 15, 12, 11, 9, 7];
+maxProfit(pricesDoge);
+
+const pricesAda = [3, 3, 3, 3, 3];
+maxProfit(pricesAda);
